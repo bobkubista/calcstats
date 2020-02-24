97a1989d46460f74339e8c54162aef8430d7392c
Bob Kubista <bob.kubista@ns.nl>
2020-02-24 11:24:01 +0100

    adding calcStats

---
+++ src/main/java/com/bobkubista/calcstats/StatsCalc.java
@@ -0,0 +1,46 @@
+package com.bobkubista.calcstats;
+
+import java.util.stream.IntStream;
+
+public class StatsCalc {
+
+    private int minimum = Integer.MIN_VALUE;
+    private int maximum = Integer.MAX_VALUE;
+    private double average = 0;
+    private int amount = 0;
+    public void calculate(int[] array) {
+            minimum = calcMin(array);
+            maximum = calcMax(array);
+            average = calcAverage(array);
+            amount = array.length;
+    }
+
+    private int calcMin(int[] array) {
+        return IntStream.of(array).min().orElse(Integer.MIN_VALUE);
+    }
+
+    private double calcAverage(int[] array) {
+        return IntStream.of(array).average().orElse(0d);
+    }
+
+    private int calcMax(int[] array) {
+        return IntStream.of(array).max().orElse(Integer.MAX_VALUE);
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public Integer amountOfItems() {
+        return amount;
+    }
+
+}
---
+++ src/test/java/com/bobkubista/calcstats/CalcStatsTest.java
@@ -0,0 +1,73 @@
+package com.bobkubista.calcstats;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalcStatsTest {
+
+    @Test()
+    public void testEmptyArray() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = {};
+            calc.calculate(array);
+        assertCalc(calc, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, 0);
+    }
+
+    @Test
+    public void testSingletonArray() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { 0 };
+        calc.calculate(array);
+        assertCalc(calc, 0, 0, 0, 1);
+    }
+
+    @Test
+    public void testSingletonMinValue() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { Integer.MIN_VALUE };
+        calc.calculate(array);
+        assertCalc(calc, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, 1);
+    }
+
+    @Test
+    public void testSingletonMaxValue() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { Integer.MAX_VALUE };
+        calc.calculate(array);
+        assertCalc(calc, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 1);
+    }
+
+    @Test
+    public void testTwoElementList() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { 1, 3 };
+        calc.calculate(array);
+        assertCalc(calc, 1, 3, 2, 2);
+    }
+
+    @Test
+    public void testTwoElementListReversed() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { 3, 1 };
+        calc.calculate(array);
+        assertCalc(calc, 1, 3, 2, 2);
+    }
+
+    private void assertCalc(StatsCalc calc, int min, int max, double avg, int length) {
+        assertEquals(min, calc.getMinimum());
+        assertEquals(max, calc.getMaximum());
+        assertEquals(avg, calc.getAverage());
+        assertEquals(length, calc.amountOfItems());
+    }
+
+
+    @Test
+    public void testLargeList() {
+        StatsCalc calc = new StatsCalc();
+        int array[] = { 6, 9, 15, -2, 92, 11 };
+        calc.calculate(array);
+        assertCalc(calc, -2, 92, 21.833333333333332, 6);
+    }
+}
