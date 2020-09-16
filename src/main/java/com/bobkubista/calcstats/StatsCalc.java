69c8edc0f453de559806a3c707f29eaa2ea96e78
Bob Kubista <b.kubista@first8.nl>
2020-09-16 18:11:06 +0200

    adding auto formatting

--- src/main/java/com/bobkubista/calcstats/StatsCalc.java
+++ src/main/java/com/bobkubista/calcstats/StatsCalc.java
@@ -4,43 +4,44 @@ import java.util.stream.IntStream;
 
 public class StatsCalc {
 
-    private int minimum = Integer.MIN_VALUE;
-    private int maximum = Integer.MAX_VALUE;
-    private double average = 0;
-    private int amount = 0;
-    public void calculate(int[] array) {
-            minimum = calcMin(array);
-            maximum = calcMax(array);
-            average = calcAverage(array);
-            amount = array.length;
-    }
+	private int minimum = Integer.MIN_VALUE;
+	private int maximum = Integer.MAX_VALUE;
+	private double average = 0;
+	private int amount = 0;
 
-    private int calcMin(int[] array) {
-        return IntStream.of(array).min().orElse(Integer.MIN_VALUE);
-    }
+	public void calculate(int[] array) {
+		minimum = calcMin(array);
+		maximum = calcMax(array);
+		average = calcAverage(array);
+		amount = array.length;
+	}
 
-    private double calcAverage(int[] array) {
-        return IntStream.of(array).average().orElse(0d);
-    }
+	private int calcMin(int[] array) {
+		return IntStream.of(array).min().orElse(Integer.MIN_VALUE);
+	}
 
-    private int calcMax(int[] array) {
-        return IntStream.of(array).max().orElse(Integer.MAX_VALUE);
-    }
+	private double calcAverage(int[] array) {
+		return IntStream.of(array).average().orElse(0d);
+	}
 
-    public int getMinimum() {
-        return minimum;
-    }
+	private int calcMax(int[] array) {
+		return IntStream.of(array).max().orElse(Integer.MAX_VALUE);
+	}
 
-    public int getMaximum() {
-        return maximum;
-    }
+	public int getMinimum() {
+		return minimum;
+	}
 
-    public double getAverage() {
-        return average;
-    }
+	public int getMaximum() {
+		return maximum;
+	}
 
-    public Integer amountOfItems() {
-        return amount;
-    }
+	public double getAverage() {
+		return average;
+	}
+
+	public Integer amountOfItems() {
+		return amount;
+	}
 
 }
